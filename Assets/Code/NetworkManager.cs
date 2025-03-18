3fbc3ea497cdf30c5e421d57345c74db4d6ea13e
N-u-j-e-e <alexander.shevelev.v@gmail.com>
2025-03-18 23:42:19 +0300

    Add: initial Photon connection

---
+++ Assets/Code/NetworkManager.cs
@@ -0,0 +1,16 @@
+using Photon.Pun;
+using UnityEngine;
+
+public sealed class NetworkManager : MonoBehaviourPunCallbacks
+{
+    private void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected to Photon Master Server");
+        PhotonNetwork.JoinLobby();
+    }
+}
